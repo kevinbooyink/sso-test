65cfdf4f83c472b457792478d78b87fdcdd9f0c5
KBooijink <k.booijink@recreate365.com>
2022-10-02 17:27:32 +0200

    add logout api.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +18,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:api')->get('/logout', function (Request $request) {
+    foreach($request->user()->tokens as $token) {
+        $token->revoke();
+    };
+    return response()->json([
+        "message" => "Logged out succesfully"
+    ]);
 });
